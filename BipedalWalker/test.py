cfb0c027afce8ef7e76a62fd7e824864f4c2d602
pricoptudor <41987455+pricoptudor@users.noreply.github.com>
2022-12-14 21:15:01 +0200

    BipedalWalker

---
+++ BipedalWalker/test.py
@@ -0,0 +1,98 @@
+import gym
+import numpy as np
+
+
+class BipedalWalker:
+	def __init__(self, render_mode='human'):
+		self.env = gym.make("BipedalWalker-v3", render_mode=render_mode)
+		self.action_space = self.env.action_space
+		self.observation_space = self.env.observation_space
+
+	def reset(self):
+		return self.env.reset()[0]
+
+	def step(self, action):
+		obs, rew, done, truncated, info = self.env.step(action)
+		return obs, rew, done
+
+	def set_state(self, state):
+		return self.env.set_state(state)
+
+	def get_state(self):
+		return self.env.get_state(self.env)
+
+	def render(self):
+		self.env.render()
+
+	def close(self):
+		self.env.close()
+
+
+def policy(state, weights):
+	# print('Weights: ', weights.shape) # weights : (4, 24)
+	# print('State: ', state.shape)  # state : (24, 1)
+	return np.matmul(weights, state) 
+
+
+def test(env, policy, weights, episodes=100, normalizer=None):
+	## policy is a function that takes (weights,state) as input and returns actions
+	rewards = []
+	for ep in range(episodes):
+		state = env.reset()
+		done = False
+		total_reward = 0.0
+		steps = 0
+
+		while not done and steps<5000:
+			env.render()
+			if normalizer:
+				state = normalizer.normalize(state)
+			action = policy(state, weights)
+			next_state, reward, done = env.step(action)
+
+			total_reward+=reward
+			steps+=1
+			state = next_state
+		
+		print(f'Episode: {ep}/{episodes}, Score: {total_reward}, Steps: {steps}')
+		rewards += [total_reward]
+
+	return rewards
+
+
+#################### Normalizing the states #################### 
+class Normalizer():
+	def __init__(self, nb_inputs):
+		self.mean = np.zeros(nb_inputs)
+		self.var = np.zeros(nb_inputs)
+
+	def restore(self):
+		self.mean = np.loadtxt('NormalizerMean.txt')
+		self.var = np.loadtxt('NormalizerVar.txt')
+
+	def normalize(self, inputs):
+		obs_mean = self.mean
+		obs_std = np.sqrt(self.var)
+		return (inputs - obs_mean) / obs_std
+
+def plot_results(rewards, episodes=100):
+	import matplotlib.pyplot as plt
+	print(f'Mean of {episodes} episodes: {np.mean(rewards)}')
+	plt.plot(rewards[0])
+	plt.title('BipedalWalker with ARS')
+	plt.xlabel('Episodes')
+	plt.ylabel('Reward per episode')
+	plt.tick_params()
+	plt.axhline(y=np.mean(rewards))
+	plt.show()
+
+
+if __name__ == '__main__':
+	weights = np.loadtxt('BipedalWalkerModel.txt')
+	env = BipedalWalker()
+	normalizer = Normalizer([1, env.observation_space.shape[0]]) ## env.observation_spave.shape == (24,)
+	normalizer.restore()
+
+	rewards = test(env, policy, weights, normalizer=normalizer)
+	
+	plot_results(np.array(rewards).reshape(1,-1))
---
+++ BipedalWalker/train.py
@@ -0,0 +1,208 @@
+import gym
+import numpy as np
+
+class BipedalWalker:
+	def __init__(self, render_mode='rgb_array'):
+		self.env = gym.make("BipedalWalker-v3", render_mode=render_mode)
+		self.action_space = self.env.action_space
+		self.observation_space = self.env.observation_space
+
+	def reset(self):
+		return self.env.reset()[0]
+
+	def step(self, action):
+		obs, reward, done, truncated, info = self.env.step(action)
+		return obs, reward, done 	
+
+	def set_state(self, state):
+		return self.env.set_state(state)
+
+	def get_state(self):
+		return self.env.get_state(self.env)
+
+	def render(self):
+		self.env.render()
+
+	def close(self):
+		self.env.close()
+
+### Hyperparameters ###
+class HP():
+	def __init__(self):
+		self.v = 0.03			# Noise in delta
+		self.N = 16				# No of perturbations
+		self.b = 16				# No of top performing directions (<= self.N)
+		self.lr = 0.02			# Learning rate
+		self.normalizer = True	# Use normalizer
+
+		self.iterations = 3_000
+		self.episode_length = 5000
+		self.test_iterations = 100
+
+class Agent:
+	def __init__(self):
+		self.hp = HP()
+
+	def policy(self, state, weights):
+		# print('Weights: ', weights.shape) # weights : (4, 24)
+		# print('State: ', state.shape)  # state : (1, 24)
+		return np.matmul(weights, state.reshape(-1,1))
+
+	def test_env(self, env, policy, weights, normalizer=None, eval_policy=False):
+		## policy is a function that takes (weights,state) as input and returns actions
+		state = env.reset()
+		done = False
+		total_reward = 0.0
+		steps = 0
+
+		while not done and steps<self.hp.episode_length:  # 5000 is episode length
+			# env.render()	## DON'T RENDER ENVIRONMENT AT TRAINING BRO
+			if normalizer:
+				if not eval_policy: normalizer.observe(state)
+				state = normalizer.normalize(state)
+			action = policy(state, weights)
+			next_state, reward, done = env.step(action)
+
+			# Avoid local optima (next_state[2] == velocity on x axis)
+			if abs(next_state[2]) < 0.001:
+				reward = -100
+				done = True
+
+			total_reward += reward
+			steps += 1
+			state = next_state
+		if eval_policy: return float(total_reward), steps # Return also the number of steps for evaluating the policy in plots
+		else: return float(total_reward)
+
+
+### Normalizing the states ###
+class Normalizer():
+	def __init__(self, nb_inputs):
+		self.n = np.zeros(nb_inputs)
+		self.mean = np.zeros(nb_inputs)
+		self.mean_diff = np.zeros(nb_inputs)
+		self.var = np.zeros(nb_inputs)
+
+	def observe(self, x):
+		self.n += 1.
+		last_mean = self.mean.copy()
+		self.mean += (x - self.mean) / self.n
+		self.mean_diff += (x - last_mean) * (x - self.mean)
+		self.var = (self.mean_diff / self.n).clip(min=1e-2)
+
+	def normalize(self, inputs):
+		obs_mean = self.mean
+		obs_std = np.sqrt(self.var)
+		return (inputs - obs_mean) / obs_std
+
+	def store(self):
+		np.savetxt('NormalizerMean.txt', self.mean)
+		np.savetxt('NormalizerVar.txt', self.var)
+
+### ARS Algorithm ###
+class ARS:
+	def __init__(self):
+		self.hp = HP()
+		self.env = BipedalWalker()
+		self.agent = Agent()
+		self.best_score = -1000
+		self.desired_score = 300
+		self.size = [self.env.action_space.shape[0], self.env.observation_space.shape[0]]
+		self.weights = np.zeros(self.size)		# 𝜃 parameters
+		if self.hp.normalizer: 
+			self.hp.normalizer = Normalizer([1,self.size[1]])
+		else: 
+			self.hp.normalizer=None
+		self.plot = ModelPlot()
+
+	def sort_directions(self, reward_p, reward_n):
+		reward_max = []
+		for rp, rn in zip(reward_p, reward_n):
+			reward_max.append(max(rp, rn))
+
+		idx = np.argsort(reward_max)	# Sort rewards and get indices.
+		idx = np.flip(idx)				# Flip to get descending order.
+
+		return idx
+
+	def update_weights(self, reward_p, reward_n, delta):
+		idx = self.sort_directions(reward_p, reward_n)
+
+		step = np.zeros(self.weights.shape)
+		for i in range(self.hp.b):
+			step += [reward_p[idx[i]] - reward_n[idx[i]]]*delta[idx[i]]
+
+		sigmaR = np.std(np.array(reward_p)[idx][:self.hp.b] + np.array(reward_n)[idx][:self.hp.b])
+		self.weights += self.hp.lr / (self.hp.b*sigmaR) * step ## TODO: delete 1.0 from paranthesis
+
+	def sample_delta(self, size):
+		return [np.random.randn(*size) for _ in range(self.hp.N)]
+
+	def save_policy(self):
+		np.savetxt('BipedalWalkerModel.txt', self.weights)
+		self.hp.normalizer.store()
+
+	def train_one_epoch(self):
+		delta = self.sample_delta(self.size)
+
+		reward_p = [self.agent.test_env(self.env, self.agent.policy, self.weights + self.hp.v*x, normalizer=self.hp.normalizer) for x in delta]
+		reward_n = [self.agent.test_env(self.env, self.agent.policy, self.weights - self.hp.v*x, normalizer=self.hp.normalizer) for x in delta]
+		
+		self.update_weights(reward_p, reward_n, delta)
+
+	def train(self):
+		print('Training started...')
+
+		for counter in range(self.hp.iterations):
+			self.train_one_epoch()
+
+			test_reward, steps = self.agent.test_env(self.env, self.agent.policy, self.weights, self.hp.normalizer, eval_policy=True)
+
+			self.plot.rewards += [test_reward]
+			self.plot.rewards_means += [np.mean(self.plot.rewards[-self.hp.test_iterations:])]
+
+			if np.mean(self.plot.rewards[-self.hp.test_iterations:]) > self.desired_score and test_reward > self.best_score:
+				## TODO: maybe also include steps here
+				self.best_score = test_reward
+				self.save_policy()
+			print(f'Iteration: {counter} -> Reward: {test_reward} || Average: {np.mean(self.plot.rewards[-self.hp.test_iterations:])}')
+			
+		print('Training ended!')
+		self.plot.plot_convergence()
+
+### Plotting convergence ###
+class ModelPlot():
+	def __init__(self):
+		self.steps = [i for i in range(HP().iterations)]
+		self.rewards = []
+		self.rewards_means = []
+
+	def plot_convergence(self):
+		import matplotlib.pyplot as plt
+		plt.plot(self.steps, self.rewards_means)
+		plt.title('Bipedal Walker -> reinforcement learning with ARS')
+		plt.xlabel('Steps')
+		plt.ylabel('Reward')
+		plt.show()
+		plt.savefig('BipedalWalkerConvergence.png')
+
+
+# Profiling app (to find bottlenecks)
+def profile_app():
+	import cProfile
+	import pstats
+
+	ars = ARS()
+	profile = cProfile.Profile()
+	profile.runcall(ars.train)
+	ps = pstats.Stats(profile)
+	ps.sort_stats('calls','cumtime')
+	ps.print_stats()
+
+if __name__ == '__main__':
+	ars = ARS()
+	ars.train()
+
+	# profile_app()
+
+	
